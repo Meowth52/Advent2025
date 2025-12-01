17fe39179fc836c1e19a46e15e78494f70166060
David Granberg <nopoe@nope.no>
2025-12-01 12:09:10 +0100

    base

--- Advent2025/Day04.cs
+++ Advent2025/Day04.cs
@@ -2,26 +2,11 @@
 {
     public class Day04 : Day
     {
-        List<int> Left;
-        List<int> Right;
+        List<int> Instructions;
         public Day04(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            List<int> instructions;
-            instructions = this.ParseListOfInteger(Input);
-            Left = new List<int>();
-            Right = new List<int>();
-            for (int i = 0; i < instructions.Count; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Left.Add(instructions[i]);
-                }
-                else
-                {
-                    Right.Add(instructions[i]);
-                }
-            }
+            Instructions = this.ParseListOfInteger(Input);
         }
         public override Tuple<string, string> GetResult()
         {
@@ -29,22 +14,13 @@
         }
         public string GetPartOne()
         {
-            Left.Sort();
-            Right.Sort();
             int ReturnValue = 0;
-            for (int i = 0; i < Left.Count; i++)
-            {
-                ReturnValue += Math.Abs(Left[i] - Right[i]);
-            }
+
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
-            for (int i = 0; i < Left.Count(); i++)
-            {
-                ReturnValue += Right.FindAll(x => x == Left[i]).Count * Left[i];
-            }
 
             return ReturnValue.ToString();
         }
