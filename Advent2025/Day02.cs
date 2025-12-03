ba2e522d6a0e9d9ebc901ea7b90d342cd1093be4
Meowten <meowth@meowth.se>
2025-12-03 16:17:59 +0100

    part 1. can clean later

--- Advent2025/Day02.cs
+++ Advent2025/Day02.cs
@@ -1,12 +1,21 @@
-﻿namespace Advent2025
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Advent2025
 {
     public class Day02 : Day
     {
-        List<int> Instructions;
+        List<(long,long)> Ranges;
         public Day02(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            string[] strings = this.ParseStringArray(Input,",");
+            Ranges = new List<(long, long)>();
+            foreach (string s in strings)
+            {
+                List<long> longs = this.ParseListOfLong(s.Replace("-",","));
+                Ranges.Add((longs[0], longs[1]));
+            }
         }
         public override Tuple<string, string> GetResult()
         {
@@ -14,8 +23,38 @@
         }
         public string GetPartOne()
         {
-            int ReturnValue = 0;
+            long ReturnValue = 0;
+            List<(long,long)> eh = new List<(long, long)>();
+            foreach ((long, long) range in Ranges)
+            {
+                int lowerD = GetDigits(range.Item1);
+                int higherD = GetDigits(range.Item2);
+                long lower = range.Item1;
+                long higher = range.Item2;
+                if (higherD % 2 == 0 || lowerD%2==0)
+                {
+                    if (lowerD % 2 != 0)
+                        lower = (long)Math.Pow(10, lowerD );
+                    if (higherD % 2 != 0)
+                        higher = (long)Math.Pow(10, higherD -1 )-1;
+                    long from = GetLeftHalf(lower);
+                    long to = GetLeftHalf(higher);
+                    for (long i = from; i < to; i++)
+                    {
+                        long palindromifiedd = Palimdromify(i);
+                        if (palindromifiedd >= lower && palindromifiedd <= higher)
+                        {
+                            ReturnValue += palindromifiedd;
+                        }
+                    }
+                    long palindromified = Palimdromify(to);
+                    if (palindromified >= lower &&palindromified <=higher)
+                    {
+                        ReturnValue += palindromified;
+                    }
+                }
 
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
@@ -23,6 +62,22 @@
             int ReturnValue = 0;
 
             return ReturnValue.ToString();
+        }
+        public int GetDigits(long number)
+        {
+            int digits = (int)(Math.Floor(Math.Log10(number)) + 1);
+            return digits;
+        }
+        public long GetLeftHalf(long number)
+        {
+            int half = GetDigits(number) / 2;
+            long returnValue = number/ (long)Math.Pow(10, half);
+            return returnValue;
+        }
+        public long Palimdromify(long number)
+        {
+            long returnValue = (number * (long)Math.Pow(10, GetDigits(number))) + number;
+            return returnValue;
         }
     }
 }
